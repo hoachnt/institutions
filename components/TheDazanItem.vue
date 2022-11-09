49b16f3e85e7c520b36899fe5277b96f451b7adb
hoach-linux <hoachprogrammer@gmail.com>
2022-11-09 18:23:31 +0500

    new features

---
+++ components/TheDazanItem.vue
@@ -0,0 +1,14 @@
+<template lang="">
+  <div>
+    <div>{{dazan.title}}</div>
+    <div>{{dazan.address}}</div>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  dazan: Object
+})
+</script>
+<style lang="">
+  
+</style>
---
+++ components/TheDazanList.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div v-for="dazan in dazans" :key="dazan.id">
+    <TheDazanItem :dazan="dazan"/>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  dazans: Array
+})
+</script>
+<style lang="">
+  
+</style>
